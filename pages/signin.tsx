cb2726ff89c77cd14ca5b475ff84e70cc06cffc9
Mateusz Ptak <ptakm02@gmail.com>
2022-02-18 21:50:09 +0100

    sign in - appearance

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,16 +1,11 @@
 import { GetServerSideProps } from 'next';
-import { getProviders, signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import React from 'react';
 import FormField from '../components/FormField';
 import useForm, { InitialStateTypes } from '../hooks/useForm';
-
-interface Provider {
-  callbackUrl: string;
-  id: string;
-  name: string;
-  signinUrl: string;
-  type: string;
-}
+import Google from '../assets/google-brands.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const initialState: InitialStateTypes = {
   name: '',
@@ -20,61 +15,46 @@ const initialState: InitialStateTypes = {
   repeatedPassword: '',
 };
 
-export default function SignIn({ providers }: { providers: Provider }) {
+export default function SignIn() {
   const { data: session } = useSession();
   const { formValues, setFormValues, handleInputChange } = useForm(initialState);
 
+  const handleSigningIn = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    await signIn('credentials', {
+      redirect: false,
+      email: formValues.email,
+      password: formValues.password,
+    });
+
+    setFormValues(initialState);
+  };
+
   return (
-    <div className="w-screen h-mobile-screen lg:h-screen flex items-center justify-center px-32 py-20">
-      <div className="grid grid-cols-[4fr_5fr] w-4/5 h-full bg-white rounded-lg shadow-2xl dark:shadow-dark">
+    <div className="w-screen h-mobile-screen xs:h-auto lg:h-screen flex items-center justify-center px-14 xs:px-32 py-20">
+      <div className="lg:grid lg:grid-cols-[4fr_5fr] w-full lg:w-4/5 h-auto lg:h-full bg-white rounded-lg shadow-2xl dark:shadow-dark">
         <div className="bg-[url('../public/images/signin.jpg')] bg-cover rounded-l-lg" />
         <div className="relative flex items-center justify-center flex-col p-5 font-['Outfit'] text-black">
-          <p className="text-4xl absolute top-8 mx-auto border-b-2 border-b-black px-7 pb-4">Sign up!</p>
-          <form className="flex flex-col">
+          <p className="text-4xl lg:absolute top-8 mx-auto border-b-2 border-b-black px-7 pb-4">Sign in!</p>
+          <form className="flex flex-col items-center mt-5 lg:mt-0" onSubmit={handleSigningIn}>
             <FormField id="email" type="email" value={formValues.email} maxLength={40} onChange={handleInputChange} />
             <FormField id="password" type="password" value={formValues.password || ''} maxLength={40} onChange={handleInputChange} />
-            <button
-              onClick={async (e) => {
-                e.preventDefault();
-                await signIn('credentials', {
-                  redirect: false,
-                  email: formValues.email,
-                  password: formValues.password,
-                });
-              }}
-            >
-              Login
-            </button>
+            <button className="bg-orange-400 text-white shadow-xl lg:hover:bg-orange-300 w-2/3 py-2 rounded-md">Login</button>
           </form>
-          {/*<button*/}
-          {/*  onClick={async () => {*/}
-          {/*    const res = await fetch('/api/signin', {*/}
-          {/*      body: JSON.stringify({*/}
-          {/*        email: 'test@gmail.com',*/}
-          {/*        password: '12345678',*/}
-          {/*        name: 'Olgierd',*/}
-          {/*        surname: 'Johnson',*/}
-          {/*      }),*/}
-          {/*      method: 'POST',*/}
-          {/*    });*/}
-          {/*  }}*/}
-          {/*>*/}
-          {/*  click*/}
-          {/*</button>*/}
-          {!session ? (
-            Object.values(providers).map(({ id, name }: { id: string; name: string }) => (
-              <button
-                key={name}
-                className="text-xl border-2 py-3 px-5 flex items-center justify-center gap-3 lg:hover:bg-gray-200"
-                onClick={() => signIn(id)}
-              >
-                Sign in with {name}
-                {/*<Image src={Google} width={20} height={20} alt="Google" />*/}
-              </button>
-            ))
-          ) : (
-            <button onClick={() => signOut()}>Sign out</button>
-          )}
+          <button
+            className="text-md mt-10 border-2 py-3 px-5 flex items-center justify-center gap-3 lg:hover:bg-gray-200"
+            onClick={() => signIn('google')}
+          >
+            Sign in with Google
+            <Image src={Google} width={30} height={30} alt="Google" />
+          </button>
+          <p className="text-sm mt-2">
+            first time here?{' '}
+            <Link href="/signup">
+              <a className="text-blue-600 hover:text-blue-400">Create an account!</a>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
@@ -82,9 +62,7 @@ export default function SignIn({ providers }: { providers: Provider }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const providers = await getProviders();
-
   return {
-    props: { providers },
+    props: {},
   };
 };
